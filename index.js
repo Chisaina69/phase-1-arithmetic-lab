f39e22036310a0aa13f158277079daed0ecceafe
Quincy Chisaina <113616008+Chisaina69@users.noreply.github.com>
2023-07-09 14:10:39 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
+const num1=2;
+const num2=31;
 
-// Write your code here
+const multiply=(num1)*(num2);
+console.log(multiply)
+
+
+const random = parseInt(Math.random(2, 1000)*100)
+console.log(random)
+const num3=22
+const num4=6
+const mod = num3 % num4
+
+
+let sets=[0,20,10,12,19]
+let nums='';
+
+console.log(...sets)
+
+let max = Math.max(...sets)
+
+console.log(max)
